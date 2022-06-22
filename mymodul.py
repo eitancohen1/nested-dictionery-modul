ca495d92e7d3d7872fd0aec8815151555dc684de
Eitan Cohen <44434461+eitancohen1@users.noreply.github.com>
2022-06-22 13:57:41 +0300

    Create mymodul.py

---
+++ mymodul.py
@@ -0,0 +1,14 @@
+myfamily={
+    1:{"id":"1","fname":"eitan","lname":"cohen","phone":"1111111"},
+    2:{"id":"2","fname":"hadas","lname":"cohen","phone":"2222222"},
+    3:{"id":"3","fname":"shaked","lname":"cohen","phone":"3333333"},
+    4:{"id":"4","fname":"uri","lname":"benyamin","phone":"4444444"},
+    5:{"id":"5","fname":"tamar","lname":"benyamin","phone":"5555555"},
+    6:{"id":"6","fname":"hagit","lname":"edri","phone":"6666666"},
+    7:{"id":"7","fname":"haim","lname":"israel","phone":"7777777"},
+    8:{"id":"8","fname":"barak","lname":"cohen","phone":"8888888"},
+    9:{"id":"9","fname":"eitan","lname":"israel","phone":"9999999"},
+    10:{"id":"10","fname":"hadas","lname":"benyamin","phone":"1111110"},
+    11:{"id":"11","fname":"barak","lname":"benyamin","phone":"1111101"},
+    
+}
